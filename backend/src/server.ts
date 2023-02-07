80fa6072bdc50cc41657ee817666ce6e1905e014
Ederson Lucas <edersonlucas@outlook.com.br>
2023-02-07 17:37:01 -0300

    refactor(backend): Change name of port env

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,7 +1,7 @@
 import 'dotenv/config';
 import app from './app';
 
-const PORT = Number(process.env.API_PORT) || 3000;
+const PORT = Number(process.env.BACKEND_PORT);
 
 const server = app.listen(PORT, () =>
   console.log(`Server is running on PORT: ${PORT}`),

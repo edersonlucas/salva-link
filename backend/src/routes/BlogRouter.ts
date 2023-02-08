7a30f9fa4df7a01c784433fe32c11ecb547ccf3a
Ederson Lucas <edersonlucas@outlook.com.br>
2023-02-07 23:34:04 -0300

    feat(backend): Create route GET /blog/tecnoblog

--- backend/src/routes/BlogRouter.ts
+++ backend/src/routes/BlogRouter.ts
@@ -3,10 +3,17 @@ import BlogController from '../controllers/BlogController';
 import AuthMiddleware from '../middlewares/AuthMiddleware';
 import TrybeBlogScraping from '../scraping/TrybeBlogScraping';
 import DevGoBlogScraping from '../scraping/DevGoBlogScraping';
+import TecnoBlogScraping from '../scraping/TecnoBlogScraping';
 
 const blogRouter = Router();
 
 blogRouter
   .get('/trybe', AuthMiddleware.auth, new BlogController(TrybeBlogScraping).get)
   .get('/devgo', AuthMiddleware.auth, new BlogController(DevGoBlogScraping).get)
+  .get(
+    '/tecnoblog',
+    AuthMiddleware.auth,
+    new BlogController(TecnoBlogScraping).get,
+  );
+
 export default blogRouter;

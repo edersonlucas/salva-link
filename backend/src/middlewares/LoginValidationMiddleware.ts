196bc0114653147f112d4f86b9bbb0b20278857c
Ederson Lucas <edersonlucas@outlook.com.br>
2023-02-08 16:28:49 -0300

    refactor(backend): Login validation middleware method name

--- backend/src/middlewares/LoginValidationMiddleware.ts
+++ backend/src/middlewares/LoginValidationMiddleware.ts
@@ -2,7 +2,7 @@ import { Request, Response, NextFunction } from 'express';
 import { validateLogin } from './validations/validateInputValues';
 
 export default class LoginValidationMiddleware {
-  public static login(req: Request, _res: Response, next: NextFunction) {
+  public static validate(req: Request, _res: Response, next: NextFunction) {
     validateLogin(req.body);
     next();
   }

e6a8273fc4502a7c5ae5d5b81212b854a4882d93
Ederson Lucas <edersonlucas@outlook.com.br>
2023-02-07 23:33:52 -0300

    feat(backend): Create route GET /blog/devgo

--- backend/src/routes/BlogRouter.ts
+++ backend/src/routes/BlogRouter.ts
@@ -2,9 +2,11 @@ import { Router } from 'express';
 import BlogController from '../controllers/BlogController';
 import AuthMiddleware from '../middlewares/AuthMiddleware';
 import TrybeBlogScraping from '../scraping/TrybeBlogScraping';
+import DevGoBlogScraping from '../scraping/DevGoBlogScraping';
 
 const blogRouter = Router();
 
 blogRouter
   .get('/trybe', AuthMiddleware.auth, new BlogController(TrybeBlogScraping).get)
+  .get('/devgo', AuthMiddleware.auth, new BlogController(DevGoBlogScraping).get)
 export default blogRouter;

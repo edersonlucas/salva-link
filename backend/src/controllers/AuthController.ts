454a69e8f163063ce601579d4180c97117b7d43d
Ederson Lucas <edersonlucas@outlook.com.br>
2023-02-07 22:51:46 -0300

    feat(backend): Create auth controller

---
+++ backend/src/controllers/AuthController.ts
@@ -0,0 +1,30 @@
+import { NextFunction, Request, Response } from 'express';
+import AuthService from '../services/AuthService';
+
+export default class AuthController {
+  private service: AuthService;
+
+  constructor() {
+    this.service = new AuthService();
+    this.login = this.login.bind(this);
+    this.register = this.register.bind(this);
+  }
+
+  public async login(req: Request, res: Response, next: NextFunction) {
+    try {
+      const token = await this.service.login(req.body);
+      return res.status(200).json({ token });
+    } catch (err) {
+      return next(err);
+    }
+  }
+
+  public async register(req: Request, res: Response, next: NextFunction) {
+    try {
+      const token = await this.service.register(req.body);
+      return res.status(200).json({ token });
+    } catch (err) {
+      return next(err);
+    }
+  }
+}

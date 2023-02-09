a5ed8357f19a2611362c28edb6da358ae6d5a020
Ederson Lucas <edersonlucas@outlook.com.br>
2023-02-09 00:53:14 -0300

    docs(backend): Create documentation with swagger ui

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,4 +1,6 @@
 import express from 'express';
+import swaggerUi from 'swagger-ui-express';
+import cors from 'cors';
 import authRouter from './routes/AuthRouter';
 import blogRouter from './routes/BlogRouter';
 import linkRouter from './routes/LinkRouter';
@@ -6,7 +8,15 @@ import ErrorMiddleware from './middlewares/ErrorMiddleware';
 
 const app = express();
 
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const swaggerDocs = require('../swagger.json');
+
+app.use(cors());
+
 app.use(express.json());
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
+
 app.use(authRouter);
 app.use('/link', linkRouter);
 app.use('/blog', blogRouter);

9419bb28093fdbd1c49b8fc6b9fc0223216ae7a4
Ederson Lucas <edersonlucas@outlook.com.br>
2023-02-09 20:10:38 -0300

    feat(frontend): Create enum to map backend register messages

--- frontend/src/enums/returnMessages.enum.ts
+++ frontend/src/enums/returnMessages.enum.ts
@@ -3,3 +3,9 @@ export enum LoginMessage {
   "Login efetuado com sucesso!" = 200,
   "Preencha todos os campos corretamente." = 422,
 }
+
+export enum RegisterMessage {
+  "Usuário cadastrado com sucesso!" = 200,
+  "Email já cadastrado." = 409,
+  "Preencha todos os campos corretamente." = 422,
+}

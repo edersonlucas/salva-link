af54124b242e720536864b23125ac3f2fe06c4df
Ederson Lucas <edersonlucas@outlook.com.br>
2023-02-10 23:17:11 -0300

    fix(frontend): Roboto font in the project

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -4,29 +4,24 @@ import type { AppProps } from "next/app";
 import { ToastContainer } from "react-toastify";
 import { AuthProvider } from "../contexts/AuthContext";
 import { GlobalProvider } from "../contexts/GlobalContext";
-import { Roboto } from "@next/font/google";
-
-const roboto = Roboto({ weight: "400", subsets: ["latin"] });
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <GlobalProvider>
       <AuthProvider>
-        <div className={roboto.className}>
-          <ToastContainer
-            position="bottom-center"
-            autoClose={2000}
-            hideProgressBar={false}
-            newestOnTop={false}
-            closeOnClick
-            rtl={false}
-            pauseOnFocusLoss
-            draggable
-            pauseOnHover
-            theme="light"
-          />
-          <Component {...pageProps} />
-        </div>
+        <ToastContainer
+          position="bottom-center"
+          autoClose={2000}
+          hideProgressBar={false}
+          newestOnTop={false}
+          closeOnClick
+          rtl={false}
+          pauseOnFocusLoss
+          draggable
+          pauseOnHover
+          theme="light"
+        />
+        <Component {...pageProps} />
       </AuthProvider>
     </GlobalProvider>
   );

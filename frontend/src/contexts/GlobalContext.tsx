d14accf9afcfb822eff01b75ea34e45161327f4c
Ederson Lucas <edersonlucas@outlook.com.br>
2023-02-10 21:43:27 -0300

    chore(frontend): Add links to global context

--- frontend/src/contexts/GlobalContext.tsx
+++ frontend/src/contexts/GlobalContext.tsx
@@ -1,3 +1,4 @@
+import ILink from "../interfaces/ILink";
 import React, {
   createContext,
   useState,
@@ -13,19 +14,24 @@ import IUser from "../interfaces/IUser";
 interface IGlobalContext {
   user: IUser | null;
   setUser: Dispatch<SetStateAction<IUser | null>>;
+  links: ILink[];
+  setLinks: Dispatch<SetStateAction<ILink[]>>;
 }
 
 export const GlobalContext = createContext({} as IGlobalContext);
 
 export function GlobalProvider({ children }: IProviderProps) {
   const [user, setUser] = useState<IUser | null>(null);
+  const [links, setLinks] = useState<ILink[]>([]);
 
   const value = useMemo(
     () => ({
       user,
       setUser,
+      links,
+      setLinks,
     }),
-    [user, setUser]
+    [user, setUser, links, setLinks]
   );
 
   return (

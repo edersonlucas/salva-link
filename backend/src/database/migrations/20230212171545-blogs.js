677a5181454f2acaec51ce1ce8b27802b58ac945
Ederson Lucas <edersonlucas@outlook.com.br>
2023-02-12 14:41:31 -0300

    feat(backend): Create blog migration

---
+++ backend/src/database/migrations/20230212171545-blogs.js
@@ -0,0 +1,29 @@
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('blogs', {
+      id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      name: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        unique: true,
+      },
+      createdAt: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      updatedAt: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+    });
+  },
+
+  down: async (queryInterface, _Sequelize) => {
+    await queryInterface.dropTable('blogs');
+  },
+};

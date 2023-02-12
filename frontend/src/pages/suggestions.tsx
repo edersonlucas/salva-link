80ef797079a7aead3a73bc402603fdaf8221c31d
Ederson Lucas <edersonlucas@outlook.com.br>
2023-02-12 12:49:30 -0300

    style(frontend): Suggestions page

--- frontend/src/pages/suggestions.tsx
+++ frontend/src/pages/suggestions.tsx
@@ -31,7 +31,7 @@ export default function Suggestions() {
         <main className="bg-blue-600 flex flex-col w-full min-h-screen grow">
           <Header />
           <div className="p-3 lg:p-7 w-full max-w-[1300px] h-full grow mx-auto">
-            <div className="flex gap-3 justify-center flex-wrap w-full h-[75vh] overflow-y-scroll scrollbar-thin scrollbar-thumb-zinc-700 scrollbar-track-zinc-600">
+            <div className="flex flex-wrap w-full h-[75vh] overflow-y-scroll scrollbar-thin scrollbar-thumb-zinc-700 scrollbar-track-zinc-600">
               <BlogCard data={{ name: 'DevGo' }} />
               <BlogCard data={{ name: 'Trybe' }} />
               <BlogCard data={{ name: 'Tecnoblog' }} />

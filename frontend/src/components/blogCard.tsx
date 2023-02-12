bd5ee5a129ef0d378220d6f57121db3ab744b69f
Ederson Lucas <edersonlucas@outlook.com.br>
2023-02-12 01:43:45 -0300

    feat(frontend): Creates the blog card component

---
+++ frontend/src/components/blogCard.tsx
@@ -0,0 +1,28 @@
+import { useState } from 'react';
+import ModalLinks from './modalLinks';
+
+interface IBlogCardProps {
+  data: {
+    name: string;
+  };
+}
+
+export default function BlogCard(props: IBlogCardProps) {
+  const { data } = props;
+  const [modalLinksIsOpen, setModalLinksIsOpen] = useState(false);
+  return (
+    <div className="flex flex-col items-center bg-white-900 h-[220px] p-4 lg:p-8 grow">
+      <h1 className="p-8 text-3xl text-blue-700 font-bold">{data.name}</h1>
+      <button
+        type="button"
+        className="bg-blue-700 mt-5 text-white-900 text-sm lg:text-base p-2 w-full rounded-sm hover:bg-blue-800 transition-colors"
+        onClick={() => setModalLinksIsOpen(true)}
+      >
+        VER SUGESTÃO
+      </button>
+      {modalLinksIsOpen && (
+        <ModalLinks data={data} setModalLinksIsOpen={setModalLinksIsOpen} />
+      )}
+    </div>
+  );
+}

c84c30cb1b50eccfaecf6dfd07ca55f7666b56f0
Ederson Lucas <edersonlucas@outlook.com.br>
2023-02-12 01:41:29 -0300

    refactor(frontend): Implements eslint suggestions on the home page

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,41 +1,31 @@
-import Head from "next/head";
-import { parseCookies } from "nookies";
-import { useEffect, useState, useContext } from "react";
-import { useRouter } from "next/router";
-import api from "../services/api";
-import { AuthContext } from "../contexts/AuthContext";
-import { GlobalContext } from "../contexts/GlobalContext";
-import Header from "../components/header";
-import LinkCard from "../components/linkCard";
-import { PlusCircle, XCircle } from "phosphor-react";
-import ModalAddLink from "../components/modalAddLink";
-import IllustrationNotFoundLink from "../assets/img/illustation-home-not-found-links.svg";
-import Image from "next/image";
-import Footer from "../components/footer";
+import Head from 'next/head';
+import { useEffect, useState, useContext } from 'react';
+import { PlusCircle, XCircle } from 'phosphor-react';
+import Image from 'next/image';
+import { AuthContext } from '../contexts/AuthContext';
+import { GlobalContext } from '../contexts/GlobalContext';
+import Header from '../components/header';
+import LinkCard from '../components/linkCard';
+import ModalAddLink from '../components/modalAddLink';
+import IllustrationNotFoundLink from '../assets/img/illustation-home-not-found-links.svg';
+import Footer from '../components/footer';
 
 export default function Home() {
-  const { push } = useRouter();
-
   const { logout, isAuthenticated } = useContext(AuthContext);
-  const { links, setLinks } = useContext(GlobalContext);
-  const [search, setSearch] = useState("");
+  const { getLinksUser, links } = useContext(GlobalContext);
+  const [search, setSearch] = useState('');
   const [modalAddIsOpen, setModalAddIsOpen] = useState(false);
 
   useEffect(() => {
-    const { "salvalink.token": token } = parseCookies();
-    if (!token) {
-      push("/login");
-    }
-    api
-      .get("/link", { headers: { Authorization: token } })
-      .then((response) => setLinks(response.data))
-      .catch((err) => {
-        const status: number = err?.response?.status;
-        if (status === 401) {
-          logout();
-        }
-      });
-  }, [logout, push, setLinks]);
+    const getLinks = async () => {
+      try {
+        await getLinksUser();
+      } catch (e) {
+        logout();
+      }
+    };
+    getLinks();
+  }, [getLinksUser, logout]);
 
   return (
     isAuthenticated && (
@@ -43,7 +33,7 @@ export default function Home() {
         <Head>
           <title>Início</title>
         </Head>
-        <main className="bg-blue-600 flex flex-col w-full min-h-screen grow">
+        <main className="bg-blue-600 flex flex-col w-screen min-h-screen grow">
           <Header />
           <div className="p-3 lg:p-7 w-full max-w-[1300px] h-full grow mx-auto">
             <div className="flex flex-col items-center mb-6 gap-3 lg:flex-row">
@@ -57,8 +47,9 @@ export default function Home() {
                 />
                 {search && (
                   <button
+                    type="button"
                     className="absolute top-2 right-2 text-zinc-700 hover:text-zinc-800 transition-colors"
-                    onClick={() => setSearch("")}
+                    onClick={() => setSearch('')}
                   >
                     <XCircle size={31} />
                   </button>
@@ -73,13 +64,13 @@ export default function Home() {
                 ADICIONAR
               </button>
             </div>
-            <div className="flex flex-col gap-3 w-full h-[60vh] lg:h-[70vh] overflow-y-scroll scrollbar-thin scrollbar-thumb-zinc-700 scrollbar-track-zinc-600">
-              {links.length > 0 ? (
+            <div className="w-full h-[60vh] lg:h-[65vh] overflow-y-scroll scrollbar-thin scrollbar-thumb-zinc-700 scrollbar-track-zinc-600">
+              {links.length ? (
                 links
                   .filter(
                     (link) =>
                       link.title.toLowerCase().includes(search.toLowerCase()) ||
-                      link.link.toLowerCase().includes(search.toLowerCase())
+                      link.link.toLowerCase().includes(search.toLowerCase()),
                   )
                   .map((link) => <LinkCard key={link.id} data={link} />)
               ) : (
